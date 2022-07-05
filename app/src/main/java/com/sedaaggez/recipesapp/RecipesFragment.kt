5f8e1741ec1e60008999686ef725144469ef8f8b
Seda <sedaaggez@aof.anadolu.edu.tr>
2022-07-05 22:55:19 +0300

    view binding

--- app/src/main/java/com/sedaaggez/recipesapp/RecipesFragment.kt
+++ app/src/main/java/com/sedaaggez/recipesapp/RecipesFragment.kt
@@ -2,16 +2,21 @@ package com.sedaaggez.recipesapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.sedaaggez.recipesapp.databinding.FragmentRecipesBinding
 
-class RecipesFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+class RecipesFragment : Fragment(R.layout.fragment_recipes) {
+    private var fragmentBinding: FragmentRecipesBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentRecipesBinding.bind(view)
+        fragmentBinding = binding
+    }
+
+    override fun onDestroy() {
+        fragmentBinding = null
+        super.onDestroy()
     }
 }
